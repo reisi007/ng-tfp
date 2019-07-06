11e91102ef8e378ab7465fe83b975a20ac925a29
Florian Reisinger <reisi007@gmail.com>
2019-07-06 08:08:26 +0200

    Revert "Print and generate PDF Button"
    This reverts commit 38aec033

--- src/app/contract/contract.component.ts
+++ src/app/contract/contract.component.ts
@@ -62,53 +62,6 @@ export class ContractComponent implements OnInit {
     this.localstorageService.setNote(value);
   }
 
-  generatePdf(): void {
-    document.body.style.width = document.body.offsetWidth + 'px';
-    let ignoredHeight = 0;
-    const printOnlyElements: Array<Element> = [];
-
-    const show: HTMLCollection = window.document.getElementsByClassName('no-screen');
-    for (let i = 0; i < show.length; i++) {
-      const cur: Element = show.item(i);
-      printOnlyElements.push(cur);
-      cur.classList.remove('no-screen');
-    }
-    console.log(printOnlyElements);
-
-    // @ts-ignore
-    const a: Promise<HTMLCanvasElement> = html2canvas(document.body, {
-      ignoreElements: (el: HTMLElement) => {
-        const shouldIgnore = el.classList.contains('no-print');
-        if (shouldIgnore) {
-          ignoredHeight += el.offsetHeight;
-        }
-        return shouldIgnore;
-      }
-    });
-    a.then(canvas => {
-      const factor = 0.75 * window.devicePixelRatio; // Magic constant
-      const img = canvas.toDataURL('image/jpeg', 0.90);
-      const factor = 0.75; // Magic constant
-      const img = canvas.toDataURL('image/jpeg', 0.9);
-      const imgElement = new Image();
-      const offset = 10;
-      imgElement.onload = () => {
-        const doc = new jsPDF({
-          orientation: 'portrait',
-          unit: 'px',
-          letterRendering: true,
-          format: [imgElement.width + 2 * offset, imgElement.height + 2 * offset - factor * ignoredHeight]
-        });
-        doc.addImage(img, 'JPEG', offset, offset - ignoredHeight * factor, imgElement.width * factor, imgElement.height * factor);
-        doc.save('Reisishot_Fotoshooting Vertrag.pdf');
-        document.body.style.width = '';
-        printOnlyElements.forEach(elem => elem.classList.add('no-screen'));
-      };
-      imgElement.src = img;
-    });
-    window.print();
-  }
-
   printPage(): void {
     window.print();
   }
